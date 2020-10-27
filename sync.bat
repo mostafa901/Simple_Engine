8438369c11604cd21b545374f35ab42b39bdee49
Moustafa Khalil <mostafa90@gmail.com>
2020-10-27 06:01:47 +0400

    Updates

---
+++ sync.bat
@@ -0,0 +1,14 @@
+git config --global http.ssl Verify false
+git add -A
+git commit -am "Updates"
+git pull --all
+git push --all -u
+
+cd /d D:\Revit_API\Projects\Shared_Library
+git add -A
+git commit -am "Updates"
+git pull --all
+git push --all -u
+
+
+pause
